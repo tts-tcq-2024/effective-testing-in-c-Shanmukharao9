c57da0c3e59e6a851718a199aa2d91d91b318dbc
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-10 18:18:08 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -27,6 +27,7 @@ int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     alertInCelcius(500);
+    alertInCelcius(-5);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
