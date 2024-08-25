e316faf95bb51799587bae6a90fe996e64895e06
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-25 22:06:33 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
+typedef struct {
+    int capturedColorCode;
+    const char* capturedMajorColor;
+    const char* capturedMinorColor;
+} colorCodeManualItem;
+
+colorCodeManualItem capturedManualList[25];
+void printColorCodeManualOnConsole(int colorCode, cont char* majorColor,const char* minorColor){
+   printf("%d | %s | %s\n", colorCode, majorColor[i], minorColor[i]);
+}
+
+int printColorMap(void (*manualPrinter)(int colorCode, const char* majorColor, const char* minorColor)) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            manualPrinter(i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
 }
 
-int main() {
-    int result = printColorMap();
+void fakeManualPrinterStub(int colorCode, const char* majorColor, const char* minorColor) {
+     (void)colorCode;
+    (void)majorColor;
+    (void)minorColor;
+    // No logic needed for the stub, just a placeholder
+}
+
+void fakeManualPrinterMock(int colorCode, const char* majorColor, const char* minorColor) {
+    colorCodeManualItem item = {colorCode, majorColor, minorColor};
+    capturedManualList[colorCode] = item;
+}
+
+void test_cases(){
+    //Value-based Testing
     assert(result == 25);
-    assert(result == 16);
-    assert(result == 24);
-    assert(result == 0);
+    assert(result > 24);
+    assert(result%5 ==0);
+
+    //interactive-based Testing
+    result = printColorMap(&fakeManualPrinterMock);
+}
+
+int main() {
+    test_cases();
+    printColorMap(&printColorCodeManualOnConsole); //Remove unused result variable
     printf("All is well (maybe!)\n");
     return 0;
 }
